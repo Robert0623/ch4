9d34b70689dbee1b1b0cf9b87bd49ae782c49ce5
Robert0623 <tone8943@gmail.com>
2022-06-15 13:57:00 +0900

    ch4-5 수정기능 구현-1 BoardController, board.jsp, boardList.jsp

--- src/main/java/com/fastcampus/ch4/controller/BoardController.java
+++ src/main/java/com/fastcampus/ch4/controller/BoardController.java
@@ -6,11 +6,14 @@ import javax.servlet.http.HttpSession;
 import com.fastcampus.ch4.domain.BoardDto;
 import com.fastcampus.ch4.domain.PageHandler;
 import com.fastcampus.ch4.service.BoardService;
+import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.HashMap;
 import java.util.List;
@@ -21,6 +24,45 @@ import java.util.Map;
 public class BoardController {
     @Autowired
     BoardService boardService;
+
+    @Autowired
+    SqlSession session;
+
+    @PostMapping("/remove")
+    public String remove(Integer bno, Integer page, Integer pageSize, Model m, HttpSession session, RedirectAttributes rattr) {
+        String writer = (String)session.getAttribute("id");
+        try {
+            m.addAttribute("page", page);
+            m.addAttribute("pageSize", pageSize);
+
+            int rowCnt = boardService.remove(bno, writer);
+
+            if (rowCnt != 1)
+                throw new Exception("board remove error");
+
+            rattr.addFlashAttribute("msg", "DEL_OK");
+        } catch (Exception e) {
+            e.printStackTrace();
+            rattr.addFlashAttribute("msg", "DEL_ERR");
+        }
+
+        return "redirect:/board/list";
+    }
+
+    @GetMapping("/read")
+    public String read(Integer bno, Integer page, Integer pageSize, Model m) {
+        try {
+            BoardDto boardDto = boardService.read(bno);
+//            m.addAttribute("boardDto", boardDto); //아래 문장과 동일
+            m.addAttribute(boardDto);
+            m.addAttribute("page", page);
+            m.addAttribute("pageSize", pageSize);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return "board";
+    }
 
     @GetMapping("/list")
     public String list(Integer page, Integer pageSize, Model m, HttpServletRequest request) {
@@ -42,6 +84,8 @@ public class BoardController {
             List<BoardDto> list = boardService.getPage(map);
             m.addAttribute("list", list);
             m.addAttribute("ph", pageHandler);
+            m.addAttribute("page", page);
+            m.addAttribute("pageSize", pageSize);
         } catch (Exception e) {
             e.printStackTrace();
         }
