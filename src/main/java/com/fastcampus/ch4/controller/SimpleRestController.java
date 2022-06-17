5f908089a3ee00efa45e565dd7e58cf7bfc26700
Robert0623 <tone8943@gmail.com>
2022-06-17 13:23:52 +0900

    ch4-9 JSON, Ajax, Jackson Databind, Rest, Rest API, Restful API

---
+++ src/main/java/com/fastcampus/ch4/controller/SimpleRestController.java
@@ -0,0 +1,33 @@
+package com.fastcampus.ch4.controller;
+
+import com.fastcampus.ch4.domain.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class SimpleRestController {
+//    @GetMapping("/ajax")
+//    public String ajax() {
+//        return "ajax";
+//    }
+
+    @PostMapping("/send")
+//    @ResponseBody
+    public Person test(@RequestBody Person p) {
+        System.out.println("p = " + p);
+        p.setName("ABC");
+        p.setAge(p.getAge() + 10);
+
+        return p;
+    }
+
+    @PostMapping("/send")
+//    @ResponseBody
+    public Person test2(@RequestBody Person p) {
+        System.out.println("p = " + p);
+        p.setName("ABC");
+        p.setAge(p.getAge() + 10);
+
+        return p;
+    }
+}
